eb49d3381e40416ad36ba09565f25f30f0bdd1e2
James <145905476+HerbalsGerbals@users.noreply.github.com>
2025-03-05 15:29:30 +1100

    Started Initial Work On Grid System And Arrow Tower

---
+++ Assets/Scripts/ArrowTower.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ArrowTower : MonoBehaviour
+{
+    public float detectionRange = 5f;
+    public LayerMask detectionLayer;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    [SerializeField] private BuildingTypeSO activeBuildingType; //Current Building
+    [SerializeField] private Transform buildingPrefab; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
