b98e10e0cf75407be4223ad67c73718539b8aaac
HerbalsGerbals <1032054@student.sae.edu.au>
2025-04-09 18:35:10 +1000

    Fixed Some UI Camera Issues

--- Assets/Scripts/Roguelike Scripts/UIManager.cs
+++ Assets/Scripts/Roguelike Scripts/UIManager.cs
@@ -5,15 +5,20 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] GameObject towerPanel;
     [SerializeField] GameObject dungeonPanel;
-    public CameraControl cameraControl;
     public bool cameraChanger = false;
     public bool slimeMovement = false;
+    [SerializeField] Canvas CanvasUI;
+    public Camera towerCam;
+    public Camera dungeonCam;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SwitchToTowerDefence();
         SlimeMovementStop();
+        towerCam.enabled = true;
+        dungeonCam.enabled = false;
+
     }
 
     public void Update()
@@ -23,24 +28,39 @@ public class UIManager : MonoBehaviour
 
     public void SwitchToTowerDefence()
     {
+        //Switch To Tower UI
         towerPanel.SetActive(true);
         dungeonPanel.SetActive(false);
-        cameraControl.ShowTowerDefence();
         cameraChanger = false;
+
+        //Stop Dungeon Enemy Movement
         SlimeMovementStop();
+
+        //Switch To Tower Camera
+        towerCam.enabled = true;
+        dungeonCam.enabled = false;
+        CanvasUI.worldCamera = towerCam;
     }
 
     public void SwitchToRoguelike()
     {
+        //Switch To Dungeon UI
         towerPanel.SetActive(false);
         dungeonPanel.SetActive(true);
-        cameraControl.ShowDungeon();
         cameraChanger = true;
+
+        //Start Dungeon Enemy Movement Again
         SlimeMovementStart();
+
+        //Switch To Dungeon Camera
+        towerCam.enabled = false;
+        dungeonCam.enabled = true;
+        CanvasUI.worldCamera = dungeonCam;
     }
 
     public void SwitchView()
     {
+        //Changes Camera Based Of Bool
         if (cameraChanger == false)
         {
             SwitchToRoguelike();
@@ -55,6 +75,7 @@ public class UIManager : MonoBehaviour
 
     public void SlimeMovementStop()
     {
+        //When Camera Isn't On Dungeon Screen Stop All Enemy Movement
         GameObject slimeEnemy = GameObject.FindWithTag("Enemy");
 
         slimeEnemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
@@ -62,6 +83,7 @@ public class UIManager : MonoBehaviour
 
     public void SlimeMovementStart()
     {
+        //When Camera Is On Dungeon Screen Start Enemy Movement After Second To Allow Player To React
         slimeMovement = true;
         if (slimeMovement == true)
         {
@@ -71,12 +93,13 @@ public class UIManager : MonoBehaviour
 
     IEnumerator SlimeMovementStartDelay()
     {
+        //Delays Enemy's Movement Starting Up By 1 Second 
         Debug.Log("Coroutine Started");
         slimeMovement = false;
         GameObject slimeEnemy = GameObject.FindWithTag("Enemy");
         yield return new WaitForSeconds(1f);
-        slimeEnemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;   
+        slimeEnemy.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
         Debug.Log("Coroutine Ended");
-        
+
     }
 }
