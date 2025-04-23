7a89513999ada518d53ad6118490d26c5745959b
HerbalsGerbals <1032054@student.sae.edu.au>
2025-04-23 21:45:45 +1000

    Finished Fixed Changes

--- Assets/Scripts/CloseGame.cs
+++ Assets/Scripts/CloseGame.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CloseGame : MonoBehaviour
 {
-    [SerializeField] private GameObject closeButton;
+    // [SerializeField] private GameObject closeButton;
 
     private void Update()
     {
